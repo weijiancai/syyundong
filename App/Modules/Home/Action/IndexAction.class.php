c015a921ab3497329ec75572c334ccc537971dc5
liu_lt <404945547@qq.com>
2015-04-30 19:24:39 +0800

    前台

--- App/Modules/Home/Action/IndexAction.class.php
+++ App/Modules/Home/Action/IndexAction.class.php
@@ -10,6 +10,7 @@ class IndexAction extends Action
     {
         $this->hotgame();
         $this->hot_activity();
+        $this->assign('city',$this->getCity());
         $this->display();
     }
 
@@ -17,21 +18,25 @@ class IndexAction extends Action
      * @功能:热门赛事排行
      * @时间：20150405
      */
-    public function hotgame(){
-        $model = D(VHotGameRanking);
-        $hot  = $model->limit(10)->select();
-        $this->assign('hot',$hot);
+    public function hotgame()
+    {
+        $model = D('VHotGameRanking');
+        $hot = $model->limit(10)->select();
+        $this->assign('hot', $hot);
     }
+
     /*
      * @功能:热门活动
      * @时间：20150405
      */
-    public function hot_activity(){
-        $hot  = D(VHotActivity)->select();
-        $this->assign('hot_activity',$hot);
+    public function hot_activity()
+    {
+        $hot = D('VHotActivity')->select();
+        $this->assign('hot_activity', $hot);
     }
+
     /*
-     * @功能注册图片验证码s
+     * @功能注册图片验证码
      * @时间：20150328
      */
     Public function verify()
@@ -39,4 +44,16 @@ class IndexAction extends Action
         import('ORG.Util.Image');
         Image::buildImageVerify(4, 1, png, 90, 30, 'verify');
     }
+
+    /*
+    * @获得城市信息
+    * @时间：20150422
+    */
+    Public function getCity()
+    {
+       return D('DbRegion')->where('pid =2 and level =3')->select();
+
+    }
+
+
 }
