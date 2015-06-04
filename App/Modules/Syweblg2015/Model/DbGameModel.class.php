c6615815d70a8324c058d1fe3a61df56d7c8d321
liu_lt <404945547@qq.com>
2015-06-04 21:08:40 +0800

    场馆修复

--- App/Modules/Syweblg2015/Model/DbGameModel.class.php
+++ App/Modules/Syweblg2015/Model/DbGameModel.class.php
@@ -4,7 +4,7 @@ class DbGameModel extends CommonModel
 {
     protected $fields = array(
         'id', 'name', 'sport_id','sport_sid', 'image', 'reg_begin_date', 'reg_end_date', 'start_date', 'end_date',
-        'limit_count', 'cost', 'province', 'address', 'sponsor', 'phone', 'apply_name', 'apply_phone',
+        'limit_count', 'cost', 'province','city','region','address', 'sponsor', 'phone', 'apply_name', 'apply_phone',
         'apply_email', 'description', 'game_declare', 'content', 'content', 'member_right', 'aout_route', 'about_cost',
         'about_trip', 'about_hotal', 'input_date', 'input_user', 'is_verify', 'verify_date',
         '_pk' => 'id', '_autoinc' => true
