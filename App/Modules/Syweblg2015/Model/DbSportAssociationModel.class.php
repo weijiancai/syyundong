4d127536c551f90e9089b96f3b48db5f25d50235
liu_lt <404945547@qq.com>
2015-06-09 07:06:55 +0800

    bug修复

--- App/Modules/Syweblg2015/Model/DbSportAssociationModel.class.php
+++ App/Modules/Syweblg2015/Model/DbSportAssociationModel.class.php
@@ -3,7 +3,7 @@
 class DbSportAssociationModel extends CommonModel
 {
     protected $fields = array(
-        'id', 'name', 'image', 'description', 'input_date', 'input_user',
+        'id', 'name', 'image','approval_time','member_count','activity_count','legal_person','mobile','address','description', 'input_date', 'input_user',
         '_pk' => 'id', '_autoinc' => true
     );
 }
