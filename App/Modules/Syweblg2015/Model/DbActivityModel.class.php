ebe77cdff95df54e23b59b648c5997438eb0e7a3
liu_lt <404945547@qq.com>
2015-06-15 06:50:23 +0800

    bug修复

--- App/Modules/Syweblg2015/Model/DbActivityModel.class.php
+++ App/Modules/Syweblg2015/Model/DbActivityModel.class.php
@@ -4,7 +4,7 @@ class DbActivityModel extends CommonModel
 {
     protected $fields = array(
         'id', 'name', 'sport_id', 'image', 'reg_begin_date', 'reg_end_date', 'start_date', 'end_date',
-        'limit_count', 'join_need_info','is_need_verify','cost_type','cost', 'province','city','region','address', 'content',
+        'limit_count', 'join_need_info','is_verify','cost_type','cost', 'province','city','region','address', 'content',
         'input_date', 'input_user', 'interest_count',
         '_pk' => 'id', '_autoinc' => true
     );
